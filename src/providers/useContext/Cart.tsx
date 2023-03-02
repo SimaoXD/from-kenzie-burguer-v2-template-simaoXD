1da5646be8533b1c98761a610deb0692d085cc62
Pedro Henrique <pedrosimao.m@gmail.com>
2023-03-02 14:57:38 -0300

    versão save

--- src/providers/useContext/Cart.tsx
+++ src/providers/useContext/Cart.tsx
@@ -1,29 +1,3 @@
-// import React, { createContext, ReactNode, useState } from "react";
-
-// interface ICartProvider {
-//   cart: object[];
-//   setCart: React.Dispatch<React.SetStateAction<object[]>>;
-//   children: React.ReactNode;
-// }
-
-// interface ICartState {
-//   cart: object[];
-//   setCart: any;
-
-// }
-
-// export const CarrinhoContext = createContext({ cart: [], setCart: () => {} });
-
-// export const CarrinhoProvider = ({ children }: ICartProvider) => {
-//   const [cart, setCart] = useState<[]>([]);
-
-//   return (
-//     <CarrinhoContext.Provider value={{ cart, setCart }}>
-//       {children}
-//     </CarrinhoContext.Provider>
-//   );
-// };
-
 import React, {
   createContext,
   ReactNode,
